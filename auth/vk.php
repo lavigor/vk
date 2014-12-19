b814727cc46f6c273f6a547059b604718b305a34
lavigor <lavigor@users.noreply.github.com>
2014-12-19 03:00:40 +0300

    Travis fixes.

--- auth/vk.php
+++ auth/vk.php
@@ -11,59 +11,59 @@ use OAuth\Common\Http\Uri\UriInterface;
 
 class Vk extends AbstractService
 {
-    public function __construct(Credentials $credentials, ClientInterface $httpClient, TokenStorageInterface $storage, $scopes = array(), UriInterface $baseApiUri = null)
-    {
-        parent::__construct($credentials, $httpClient, $storage, $scopes, $baseApiUri);
-        if( null === $baseApiUri ) {
-            $this->baseApiUri = new Uri('https://api.vk.com/method/');
-        }
-    }
-    
-    /**
-     * @return \OAuth\Common\Http\Uri\UriInterface
-     */
-    public function getAuthorizationEndpoint()
-    {
-        return new Uri('https://oauth.vk.com/authorize');
-    }
+	public function __construct(Credentials $credentials, ClientInterface $httpClient, TokenStorageInterface $storage, $scopes = array(), UriInterface $baseApiUri = null)
+	{
+		parent::__construct($credentials, $httpClient, $storage, $scopes, $baseApiUri);
+		if( null === $baseApiUri ) {
+			$this->baseApiUri = new Uri('https://api.vk.com/method/');
+		}
+	}
 
-    /**
-     * @return \OAuth\Common\Http\Uri\UriInterface
-     */
-    public function getAccessTokenEndpoint()
-    {
-        return new Uri('https://oauth.vk.com/access_token');
-    }
+	/**
+	* @return \OAuth\Common\Http\Uri\UriInterface
+	*/
+	public function getAuthorizationEndpoint()
+	{
+		return new Uri('https://oauth.vk.com/authorize');
+	}
 
-    /**
-     * @param string $responseBody
-     * @return \OAuth\Common\Token\TokenInterface|\OAuth\OAuth2\Token\StdOAuth2Token
-     * @throws \OAuth\Common\Http\Exception\TokenResponseException
-     */
-    protected function parseAccessTokenResponse($responseBody)
-    {
-	$data = json_decode($responseBody, true); 
+	/**
+	* @return \OAuth\Common\Http\Uri\UriInterface
+	*/
+	public function getAccessTokenEndpoint()
+	{
+		return new Uri('https://oauth.vk.com/access_token');
+	}
 
-        if( null === $data || !is_array($data) ) {
-            throw new TokenResponseException('Unable to parse response.');
-        } elseif( isset($data['error'] ) ) {
-            throw new TokenResponseException('Error in retrieving token: "' . $data['error'] . '"');
-        }
+	/**
+	* @param string $responseBody
+	* @return \OAuth\Common\Token\TokenInterface|\OAuth\OAuth2\Token\StdOAuth2Token
+	* @throws \OAuth\Common\Http\Exception\TokenResponseException
+	*/
+	protected function parseAccessTokenResponse($responseBody)
+	{
+		$data = json_decode($responseBody, true);
 
-        $token = new StdOAuth2Token();
+		if( null === $data || !is_array($data) ) {
+			throw new TokenResponseException('Unable to parse response.');
+		} elseif( isset($data['error'] ) ) {
+			throw new TokenResponseException('Error in retrieving token: "' . $data['error'] . '"');
+		}
 
-        $token->setAccessToken( $data['access_token'] );
-        $token->setLifeTime( $data['expires_in'] );
+		$token = new StdOAuth2Token();
 
-        if( isset($data['refresh_token'] ) ) {
-            $token->setRefreshToken( $data['refresh_token'] );
-            unset($data['refresh_token']);
-        }
+		$token->setAccessToken( $data['access_token'] );
+		$token->setLifeTime( $data['expires_in'] );
 
-        unset( $data['access_token'] );
-        unset( $data['expires_in'] );
-        $token->setExtraParams( $data );
-	
-        return $token;
-    }
+		if( isset($data['refresh_token'] ) ) {
+			$token->setRefreshToken( $data['refresh_token'] );
+			unset($data['refresh_token']);
+		}
+
+		unset( $data['access_token'] );
+		unset( $data['expires_in'] );
+		$token->setExtraParams( $data );
+
+		return $token;
+	}
 }
